bbd58611ac1d2595c928899cbf2d1bacaa0f8604
Jan Mlatec <tahicz@gmail.com>
2024-08-26 13:20:21 +0200

    Currency formating at dashboard

--- src/Controller/Admin/AdditionalFeeCrudController.php
+++ src/Controller/Admin/AdditionalFeeCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\AdditionalFee;
 use App\Enum\AdditionalFeeEnum;
 use App\Enum\PaymentFrequencyEnum;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
@@ -33,7 +34,8 @@ class AdditionalFeeCrudController extends AbstractCrudController
         yield ChoiceField::new('paymentFrequency')
             ->setChoices(PaymentFrequencyEnum::choices())
             ->setRequired(true);
-        yield BooleanField::new('billable');
+        yield BooleanField::new('billable')
+            ->renderAsSwitch(in_array($pageName, [Crud::PAGE_NEW, Crud::PAGE_EDIT], true));
         yield DateTimeField::new('createdAt')
             ->hideOnForm();
         yield DateTimeField::new('updatedAt')
