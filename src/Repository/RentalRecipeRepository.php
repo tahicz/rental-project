eeac5c3224c94015b06e79ff89c234198ffb0157
Jan Mlatec <tahicz@gmail.com>
2024-08-28 01:54:15 +0200

    Cleanup repositories

--- src/Repository/RentalRecipeRepository.php
+++ src/Repository/RentalRecipeRepository.php
@@ -15,29 +15,4 @@ class RentalRecipeRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, RentalRecipe::class);
     }
-
-    //    /**
-    //     * @return RentalRecipe[] Returns an array of RentalRecipe objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('r.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?RentalRecipe
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
